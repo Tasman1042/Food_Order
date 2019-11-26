b81aa98c05ae8dd0e64e0700abc8f350cf004dcf
Tasman Parnell <tasmanparnell@Tasmans-Air.myob.myobcorp.net>
2019-11-26 15:07:29 +1100

    first commit

---
+++ Food_Order/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Food_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
